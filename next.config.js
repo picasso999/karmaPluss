fc4866684384a88bc32619ade75c9ba1c9730f08
Puranam Pradeep Picasso <pichupicasso@gmail.com>
2021-02-16 18:34:44 +0530

    changed next.cofig.js to redirect webpage

--- next.config.js
+++ next.config.js
@@ -1,2 +1,11 @@
-const withImages = require('next-images')
-module.exports = withImages();
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://www.karmapluss.in/',
+        permanent: true,
+      },
+    ]
+  },
+}
